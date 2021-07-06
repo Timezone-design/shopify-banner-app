f16f6cb47bdfef6d0e6d761b6a74f5fdcb1f3afb
Timezonedesign <bolesalavb@gmail.com>
2021-07-06 02:03:07 -0700

    shopify banner custom app

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import { Heading, Page, TextStyle, Layout, EmptyState} from "@shopify/polaris";
+
+const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
+
+const Index = () => (
+  <Page>
+    <Layout>
+      <EmptyState // Empty state component
+        heading="Banner Message"
+        action={{
+          content: 'Edit',
+          onAction: () => this.setState({ open: true }),
+        }}
+        image={img}
+      >
+        <p>Edit your banner message and plan to publish.</p>
+      </EmptyState>
+    </Layout>
+  </Page>
+);
+
+export default Index;
